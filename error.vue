ad2d8541ae0bf11da81ac42c6dc3960edc7e5ab9
Cyril VERLOOP <cyril.verloop@webqard.fr>
2023-11-18 12:21:34 +0100

    Adding files to the project.

---
+++ error.vue
@@ -0,0 +1,8 @@
+<script setup>
+    defineProps(['error']);
+</script>
+
+<template>
+    {{ error.statusCode }} - {{ error.message }}<br />
+    <NuxtLink to="/">Go back home</NuxtLink>
+</template>
