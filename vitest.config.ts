3fbdf1c8526d52194d0cd9971999605e98ecbca3
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-09-04 14:04:10 +0200

    Configuring coverage report directory.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineVitestConfig } from '@nuxt/test-utils/config';
 
 export default defineVitestConfig({
     test: {
+        coverage: {
+            reportsDirectory: '../ci/coverage/'
+        },
         environment: 'nuxt'
     }
 });
