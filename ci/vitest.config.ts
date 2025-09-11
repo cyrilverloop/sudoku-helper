0d3c940f8cb8a24021d12e9709cab308e2b2895e
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-09-11 14:17:07 +0200

    Moving "vitest.config.ts" to "./ci/".

--- ci/vitest.config.ts
+++ ci/vitest.config.ts
@@ -3,8 +3,14 @@ import { defineVitestConfig } from '@nuxt/test-utils/config';
 export default defineVitestConfig({
     test: {
         coverage: {
+            reporter: [
+                ["html", {subdir: "./html/"}],
+            ],
             reportsDirectory: '../ci/coverage/'
         },
-        environment: 'nuxt'
+        environment: 'nuxt',
+        include: [
+            "./test/**/*.test.ts"
+        ]
     }
 });
